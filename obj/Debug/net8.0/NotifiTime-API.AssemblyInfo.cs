3ad8e7e63d4931ae8521c51bd6d764216c2f65e8
MiguelAngelHitaAcevedo <miguelangelhitaacev@gmail.com>
2025-03-08 13:41:17 +0100

    Fix: gitignore updated

--- obj/Debug/net8.0/NotifiTime-API.AssemblyInfo.cs
+++ obj/Debug/net8.0/NotifiTime-API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NotifiTime-API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82ff13647bc49ec322934cc1ffd84327d1b98fa2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+650a94c8e3e2029f75a52a4b39872f5ed328ab6a")]
 [assembly: System.Reflection.AssemblyProductAttribute("NotifiTime-API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NotifiTime-API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
