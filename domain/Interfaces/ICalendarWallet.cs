aa4d2f293837619b15275ebd2822f790fbd0d9bb
MiguelAngelHitaAcevedo <miguelangelhitaacev@gmail.com>
2025-03-14 19:52:17 +0100

    Feature: ICalendarWallet

---
+++ domain/Interfaces/ICalendarWallet.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NotifiTime_API.domain.entities;
+
+namespace NotifiTime_API.domain.Interfaces
+{
+    public interface ICalendarWallet
+    {
+        public Exception addCalendarNotifiTime(ICalendarNotifiTime newCalendarNotifiTime);
+        public Exception deleteCalendarNotifiTimeById(Guid id);
+        public ICalendarEvent findCalendarEventByIdOnAllCalendars(Guid eventId);
+        public ICalendarNotifiTime findCalendarNotifiTimeById(Guid id);
+        public ICalendarNotifiTime[] findCalendarNotifiTimeByName(string name);
+        public ICalendarNotifiTime[] getCalendarNotifiTimeArray();
+        public ICalendarNotifiTime[] sortCalendarNotifiTimeListByCreationDate(bool ascending);
+        public ICalendarNotifiTime[] sortCalendarNotifiTimeListByName(bool ascending);
+        public Exception updateCalendarsNotifiTime();
+    }
+}
