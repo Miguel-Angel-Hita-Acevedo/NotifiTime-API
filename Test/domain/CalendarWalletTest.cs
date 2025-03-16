98b94729100fae28ea2d1083c78b8e2fd6709ba0
MiguelAngelHitaAcevedo <miguelangelhitaacev@gmail.com>
2025-03-16 18:20:16 +0100

    Test feature: test name methods with all expected coverage on CalendarWalletTest

---
+++ Test/domain/CalendarWalletTest.cs
@@ -0,0 +1,107 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace NotifiTime_API.Test.domain
+{
+    public class CalendarWalletTest
+    {
+        [Fact]
+        public void addCalendarNotifiTimeToEmptyDictionaryReturnTrue()
+        {
+            
+        }
+        
+        [Fact]
+        public void addCalendarNotifiTimeToDictionaryWithExistingGuidReturnFalse()
+        {
+            
+        }
+        
+        [Fact]
+        public void addCalendarNotifiTimeToDictionaryWithOneDifferentElementReturnTrue()
+        {
+            
+        }
+        
+        [Fact]
+        public void deleteCalendarNotifiTimeByIdThatExistsReturnTrue()
+        {
+            
+        }
+        
+        [Fact]
+        public void deleteCalendarNotifiTimeByIdThatDoesntExistsReturnFalse()
+        {
+            
+        }
+        
+        [Fact]
+        public void findCalendarEventsByIdOn3CalendarsThatExists()
+        {
+            
+        }
+        
+        [Fact]
+        public void findCalendarEventsByIdOn3CalendarsThatDoesntExists()
+        {
+            
+        }
+        
+        [Fact]
+        public void findCalendarEventsByIdOn3CalendarsAndFound2EventsOnDifferentCalendars()
+        {
+            
+        }
+        
+        [Fact]
+        public void findCalendarNotifiTimeByIdThatExistsReturnElement()
+        {
+            
+        }
+        
+        [Fact]
+        public void findCalendarNotifiTimeByIdThatDoesntExistsReturnNull()
+        {
+            
+        }
+        
+        [Fact]
+        public void findCalendarNotifiTimeByNameThatExistsReturnElement()
+        {
+            
+        }
+        
+        [Fact]
+        public void findCalendarNotifiTimeByNameThatDoesntExistsReturnNull()
+        {
+            
+        }
+
+        [Fact]
+        public void sortCalendarNotifiTimeByCreationDateAscending()
+        {
+            
+        }
+        
+        [Fact]
+        public void sortCalendarNotifiTimeByCreationDateDescending()
+        {
+            
+        }
+        
+        [Fact]
+        public void sortCalendarNotifiTimeByNameAscending()
+        {
+            
+        }
+        
+        [Fact]
+        public void sortCalendarNotifiTimeByNameDescending()
+        {
+            
+        }
+    }
+}
