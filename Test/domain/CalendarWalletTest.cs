732d239ea0cf643794f36c123d43d42ac4f826be
MiguelAngelHitaAcevedo <miguelangelhitaacev@gmail.com>
2025-03-21 18:17:21 +0100

    Feature Test: addCalendarNotifiTimeToEmptyDictionaryReturnTrue and addCalendarNotifiTimeToDictionaryWithExistingGuidReturnFalse on CalendarWalletTest passing

--- Test/domain/CalendarWalletTest.cs
+++ Test/domain/CalendarWalletTest.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using NotifiTime_API.domain.entities;
+using NotifiTime_API.domain.Interfaces;
 using Xunit;
 
 namespace NotifiTime_API.Test.domain
@@ -11,13 +13,30 @@ namespace NotifiTime_API.Test.domain
         [Fact]
         public void addCalendarNotifiTimeToEmptyDictionaryReturnTrue()
         {
+            ICalendarWallet calendarWallet = new CalendarWallet();
+            ICalendarNotifiTime calendarNotifiTime = new CalendarNotifiTime("test calendar");
             
+            bool created = calendarWallet.addCalendarNotifiTime(calendarNotifiTime);
+            
+            Assert.True(created);
         }
         
         [Fact]
         public void addCalendarNotifiTimeToDictionaryWithExistingGuidReturnFalse()
         {
+            ICalendarWallet calendarWallet = new CalendarWallet();
+            ICalendarNotifiTime firstCalendarNotifiTime = new CalendarNotifiTime("first test calendar");
+            ICalendarNotifiTime secondCalendarNotifiTime = new CalendarNotifiTime(
+                firstCalendarNotifiTime.getId(), 
+                "second test calendar",
+                (new Dictionary<Guid, CalendarEvent>()),
+                firstCalendarNotifiTime.getCreationDate()
+            );
             
+            calendarWallet.addCalendarNotifiTime(firstCalendarNotifiTime);
+            bool created = calendarWallet.addCalendarNotifiTime(secondCalendarNotifiTime);
+            
+            Assert.False(created);
         }
         
         [Fact]
