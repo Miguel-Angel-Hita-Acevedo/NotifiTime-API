54207553787c67d2b79aaba93458fafabcdc3800
MiguelAngelHitaAcevedo <miguelangelhitaacev@gmail.com>
2025-07-02 17:38:59 +0200

    Feature: infrastructure.entities.CalendarNotifiTimeEntity completed

--- infrastructure/entities/CalendarNotifiTimeEntity.cs
+++ infrastructure/entities/CalendarNotifiTimeEntity.cs
@@ -8,5 +8,14 @@ namespace NotifiTime_API.infrastructure.entities
     public class CalendarNotifiTimeEntity
     {
         
+        private Guid id;
+        private string name;
+        private Dictionary<Guid, EventCalendarEntity> eventCalendarDictionary;
+        private DateTime creationDate;
+
+        public Guid Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+        public Dictionary<Guid, EventCalendarEntity> EventCalendarDictionary { get => eventCalendarDictionary; set => eventCalendarDictionary = value; }
+        public DateTime CreationDate { get => creationDate; set => creationDate = value; }
     }
 }
