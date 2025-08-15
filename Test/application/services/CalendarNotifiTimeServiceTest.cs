e317acf7285c073ba83bd5dab9647e1156e6b03c
Miguel Ángel Hita Acevedo <miguelangelhitaacev@gmail.com>
2025-08-15 18:38:14 +0200

    Feature: added one test to CalendarNotifiTimeServiceTest

--- Test/application/services/CalendarNotifiTimeServiceTest.cs
+++ Test/application/services/CalendarNotifiTimeServiceTest.cs
@@ -2,6 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using NotifiTime_API.application.DTOs;
+using NotifiTime_API.application.services;
+using NotifiTime_API.domain.entities;
+using NotifiTime_API.domain.Enum;
 using Xunit;
 
 namespace NotifiTime_API.Test.application
@@ -9,9 +13,24 @@ namespace NotifiTime_API.Test.application
     public class CalendarNotifiTimeServiceTest
     {
         [Fact]
-        public void Test1()
+        public void CreateEvent_FullParametersOfEvent_ReturnFullParametersOnDto()
         {
-            Assert.True(true);
+            CalendarNotifiTime calendar = new CalendarNotifiTime("Test calendar");
+            CalendarNotifiTimeService calendarService = new CalendarNotifiTimeService(calendar);
+
+            EventCalendarDTO eventCalendarDto = calendarService.
+                CreateEvent(
+                    new DateTime(10, 10, 10),
+                    "Test event",
+                    TimeIteration.Annually
+                );
+            
+            
+            Assert.True(
+                    eventCalendarDto.DateTime == new DateTime(10, 10, 10) &&
+                    eventCalendarDto.Name == "Test event" &&
+                    eventCalendarDto.TimeIteration == TimeIteration.Annually
+                );
         }
     }
 }
